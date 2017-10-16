c48900457992ee91a0d1ccb31dd9715826ce6be5
Ryan Fisher <ryancfisher@gmail.com>
2017-10-15 22:51:05 -0700

    Add white_house method to compare distance between other addresses

--- lib/presenters/address_presenter/collection.rb
+++ lib/presenters/address_presenter/collection.rb
@@ -10,11 +10,23 @@ class AddressPresenter
       [35.109937, -89.959983]
     ]
 
+    WHITE_HOUSE_ADDRESS =
+      "1600 Pennsylvania Avenue NW Washington, D.C. 20500"
+
     def initialize
       @collection = COORDS.map do |coords|
         address = Address.new
         address.coords = coords
         AddressPresenter.new(address)
+      end
+    end
+
+    def white_house
+      @_white_house ||= begin
+        address = Address.new
+        address.full_address = WHITE_HOUSE_ADDRESS
+
+        address
       end
     end
 
---
+++ spec/lib/address_presenter/collection_spec.rb
@@ -0,0 +1,16 @@
+RSpec.describe AddressPresenter::Collection do
+  subject(:collection) { described_class.new }
+
+  let(:payload) { { 'latt' => '', 'longt' => '' } }
+  let(:result) { [double(data: payload)] }
+
+  before do
+    allow(Geocoder).to receive(:search) { result }
+  end
+
+  describe '#white_house' do
+    it 'returns an Address' do
+      expect(collection.white_house).to be_instance_of Address
+    end
+  end
+end
